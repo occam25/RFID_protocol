a258710f6a1ba732cd056ea29d7f2d457ef7c28f
JaP <occam25@yahoo.es>
2020-03-12 20:30:24 +0100

    finish algorithm

--- src/main.c
+++ src/main.c
@@ -20,39 +20,39 @@ static uint64_t server_certificate;
 int main(void) {
 
 	// Step 1: Server certificate request
-	printf("Getting server certificate...\n");
+	printf("### Getting certificate from the server...\n");
 	uint8_t result;
 	result = server_certificate_request(&server_certificate);
 	if(result != 0){
-		fprintf(stderr, "Server authentication failed\n");
+		fprintf(stderr, "Server authentication failed!\n");
 		return 1;
 	}
-	printf("CERT: %lX\n", server_certificate);
+	printf("CERT: \t\t%lX\n", server_certificate);
 
 	// Step 2: Active Pid request
 	// First try to get pid2
-	printf("Requesting tag's Pid...\n");
+	printf("\n### Requesting tag's Pid...\n");
 	result = tag_request(0, &active_pid);
 	if(result != 0){
 		fprintf(stderr, "Pid request failed\n");
 		return 1;
 	}
-	printf("Current Pid: %lX\n",active_pid);
+	printf("Current Pid: \t%lX\n",active_pid);
 
 	// Step 3: Request keys to the server, generate n1 and n2 and compute A, B and D
 	// Request keys
-	printf("Requesting keys...\n");
+	printf("\n### Requesting keys from server...\n");
 	result = server_keys_request(active_pid, server_certificate, &k1, &k2);
 
 	if(result != 0){
 		fprintf(stderr, "Keys request failed\n");
 		return 1;
 	}
-	printf("K1: %lX\n",k1);
-	printf("K2: %lX\n",k2);
+	printf("Reader K1: \t%lX\n",k1);
+	printf("Reader K2: \t%lX\n",k2);
 
 	// Random numbers
-	printf("Generating random numbers...\n");
+	printf("\n### Generating random numbers...\n");
 	uint64_t n1;
 	uint64_t n2;
 	srand(time(NULL));
@@ -69,11 +69,11 @@ int main(void) {
 	     n2 |= k;
 	}
 
-	printf("n1: %lX\n",n1);
-	printf("n2: %lX\n",n2);
+	printf("Reader n1: \t%lX\n",n1);
+	printf("Reader n2: \t%lX\n",n2);
 
 	// Compute A, B and D
-	printf("A, B and D computation...\n");
+	printf("\n### A, B and D computation...\n");
 	uint64_t A;
 	uint64_t B;
 	uint64_t D;
@@ -82,10 +82,42 @@ int main(void) {
 	B = (~active_pid & k2 & k1) ^ n2;
 	D = (k1 & n2) ^ (k2 & n1);
 
-	printf("A: %lX\n",A);
-	printf("B: %lX\n",B);
-	printf("D: %lX\n",D);
+	printf("Reader A: \t%lX\n",A);
+	printf("Reader B: \t%lX\n",B);
+	printf("Reader D: \t%lX\n",D);
 
+	uint64_t E;
+	uint64_t F;
+
+	printf("Reader: sending A, B and D to the tag...\n");
+	result = tag_compute_E_F(A, B, D, &E, &F);
+
+	if(result != 0){
+		printf("\nReader authentication failed\n");
+		return 1;
+	}
+
+	printf("\n### Reader authentication succeeded!! Checking Tag...\n");
+
+	uint64_t computed_F = (k1 & n1) ^ (k2 & n2);
+	printf("Reader F: \t%lX\n",computed_F);
+
+	if(computed_F != F){
+		printf("Tag authentication failed\n");
+		return 1;
+	}
+
+	printf("Tag authentication succeeded!\n");
+
+	printf("\n### Computing ID and updating pids\n");
+
+	id = E ^ k1 ^ n1 ^ (k2 & n2);
+	pid = pid2;
+	pid2 = pid2 ^ n1 ^ n2;
+
+	printf("Reader ID: \t%lX\n", id);
+	printf("Reader pid: \t%lX\n", pid);
+	printf("Reader pid2: \t%lX\n", pid2);
 
 	return EXIT_SUCCESS;
 }
--- src/tag.c
+++ src/tag.c
@@ -2,13 +2,19 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
 #include "tag.h"
 
 static uint64_t id =  0xE1D896E4B5A90B18;
 static uint64_t pid = 0x01EEF785A7CD9001;
 static uint64_t pid2= 0x025EF9877ABB1C8D;
+static uint64_t active_pid;
 static uint64_t k1 =  0xA1B2C3D4E5F60102;
 static uint64_t k2 =  0xF1E2D3C4B5A69788;
+
+static uint64_t n1;
+static uint64_t n2;
 
 uint8_t tag_request(uint8_t second_try, uint64_t *pid_to_use)
 {
@@ -20,6 +26,41 @@ uint8_t tag_request(uint8_t second_try, uint64_t *pid_to_use)
 	else
 		*pid_to_use = pid2;
 
+	active_pid = *pid_to_use;
+
 	return 0;
 
+}
+
+uint8_t tag_compute_E_F(uint64_t A, uint64_t B, uint64_t D, uint64_t *E, uint64_t *F)
+{
+
+	printf("\n### TAG: computing parameters...\n");
+//	n1 = A ^ (active_pid & k1);
+//	n2 = B ^ (~active_pid & k2);
+
+	n1 = A ^ (active_pid & k1 & k2);
+	n2 = B ^ (~active_pid & k2 & k1);
+
+	uint64_t computed_D = (k1 & n2) ^ (k2 & n1);
+
+	printf("TAG n1: \t%lX\n",n1);
+	printf("TAG n2: \t%lX\n",n2);
+	printf("TAG D: \t\t%lX\n",computed_D);
+
+	if(computed_D != D)
+		return 1;
+
+	*E = (k1 ^ n1 ^ id) ^ (k2 & n2);
+	*F = (k1 & n1) ^ (k2 & n2);
+	pid = pid2;
+	pid2 = pid2 ^ n1 ^ n2;
+
+	printf("TAG F: \t\t%lX\n",*F);
+	printf("TAG E: \t\t%lX\n",*E);
+	printf("TAG ID: \t%lX\n", id);
+	printf("TAG pid: \t%lX\n",pid);
+	printf("TAG pid2: \t%lX\n",pid2);
+
+	return 0;
 }
